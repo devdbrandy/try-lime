5cfc20edb7e596f494e520ee71120d29cc97b4d7
Harry Dbrandy <devdbrandy@gmail.com>
2022-04-01 20:47:20 +0100

    style: abstract navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SwiperModule } from 'swiper/angular';
-
-import { AppRoutingModule } from './app-routing.module';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { SimpleModalModule } from 'ngx-simple-modal';
 import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HotelSliderComponent } from './components/hotel-slider/hotel-slider.component';
 import { MapComponent } from './components/map/map.component';
 import { HotelCardComponent } from './components/hotel-card/hotel-card.component';
 import { BookingFormComponent } from './components/booking-form/booking-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MapComponent,
     HotelCardComponent,
     BookingFormComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
